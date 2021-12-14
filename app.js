06f86f3720756460e6a6bccbff7e9ce6c7e96517
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-14 10:54:29 +0100

    countdown minute and second core

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 let playStop = document.getElementById('play');
 let reset = document.getElementById('reset');
-let result =  document.getElementById('result');
+let resultSecond =  document.getElementById('resultSecond');
 let countSecond = document.getElementById('inputSecond');
 let timeOutId = null;
+let countMinute = document.getElementById('inputMinute');
+let resultMinute =  document.getElementById('resultMinute');
 
 
-//make a chrono for a second
-let countdown = function () {
-    timeOutId = setTimeout(function () {
-        countSecond.value = countSecond--;
-        result.innerHTML = countSecond;
-        countdown();
-        return timeOutId;
-    },1000)
-}
+playStop.addEventListener('click', function timeOutId() {
+    let chrono = setTimeout(timeOutId,1000 ,countSecond.value);
+    countSecond.value--;
+    resultSecond.innerHTML = countSecond.value;
+    resultMinute.innerHTML = countMinute.value;
 
-playStop.addEventListener('click' ,() => {
-    countdown();
-    console.log(countSecond.parseInt)
-});
+    //check si la value des minute et vite ou inférieur a 1 et seconde inférieur a 1 alors 'clearTimeOut'
+    if (countMinute.value === '' || countMinute.value <= 0 && countSecond.value <= 1 ) {
+            clearTimeout(chrono);
+    }
+    //si les minutes contient une valeur alors:
+    else {
+        // une fois arriver a la derniere seconde repart a 60 et retire 1 minute
+        if (countSecond.value < 1 ) {
+            countSecond.value = 60;
+            let chronoMinute = setTimeout(timeOutId,60000 ,countMinute.value);
+            countMinute.value--;
+        }
+    }//balise ferment premier else
+
+})
