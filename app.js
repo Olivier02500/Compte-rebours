cb920e46008b3d0fa8a538ce7749349f23cac32e
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-14 14:14:31 +0100

    countdown JS core is finish

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-let playStop = document.getElementById('play');
+let play = document.getElementById('play');
 let reset = document.getElementById('reset');
 let resultSecond =  document.getElementById('resultSecond');
 let countSecond = document.getElementById('inputSecond');
-let timeOutId = null;
 let countMinute = document.getElementById('inputMinute');
 let resultMinute =  document.getElementById('resultMinute');
+let stop = document.getElementById('stop');
 
 
-playStop.addEventListener('click', function timeOutId() {
+    play.addEventListener('click', function timeOutId() {
     let chrono = setTimeout(timeOutId,1000 ,countSecond.value);
     countSecond.value--;
     resultSecond.innerHTML = countSecond.value;
     resultMinute.innerHTML = countMinute.value;
 
     //check si la value des minute et vite ou inférieur a 1 et seconde inférieur a 1 alors 'clearTimeOut'
-    if (countMinute.value === '' || countMinute.value <= 0 && countSecond.value <= 1 ) {
+    if (countMinute.value === '' && countSecond.value <= 0 || countMinute.value <= 0 && countSecond.value <= 0 ) {
             clearTimeout(chrono);
     }
     //si les minutes contient une valeur alors:
     // une fois arriver a la derniere seconde repart a 60 et retire 1 minute
     else {
-        if (countSecond.value < 1 ) {
+        if (countSecond.value < 0 ) {
             countSecond.value = 60;
             let chronoMinute = setTimeout(timeOutId,60000 ,countMinute.value);
             countMinute.value--;
         }
     }
 
-    playStop.addEventListener('click', function() {
+    stop.addEventListener('click' ,function (){
         clearTimeout(chrono);
-    })
-})
+    });
+
+    reset.addEventListener('click' ,function (){
+        countSecond.value = '';
+        countMinute.value = '';
+    });
+
+});
 
